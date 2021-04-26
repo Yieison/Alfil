d38471e04ba9f1876d6f794b4767759493a426a4
Marco Antonio Adarme Jaimes <madarme@ufps.edu.co>
2021-04-26 13:06:12 -0500

    Clases iniciales proyecto alfil

---
+++ src/Negocio/Tablero.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+import Modelo.Ficha;
+
+/**
+ *
+ * @author madar
+ */
+public class Tablero {
+    
+    
+    private Ficha [][]myTablero;
+    //Adicione los atributos que considere necesarios para el correcto fucnioanmiento de su aplicación, si y solo si , no violen ninguna regla en POO
+    public Tablero() {
+    }
+    
+    /**
+     * Constructor para iniciar el juego
+     * @param i_alfil Posición de la fila para el alfil
+     * @param j_alfil Posición de la columna para el alfil
+     * @param i_peon  Posición de la fila para el peon
+     * @param j_peon  Posición de la columna para el alfil
+     * @param dirPeon true si el peón se mueve de arriba hacia abajo, o false en caso contrario
+     */
+    
+     public Tablero(int i_alfil, int j_alfil, int i_peon,int j_peon, boolean dirPeon) {
+         
+         
+    }
+    
+    
+    public String jugar()
+    {
+        //:)
+        
+        return null;
+    }
+}
